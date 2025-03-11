6d510e0af2dd046606283965b35f6d72376cb894
david-barrei <davbarreiro4@gmail.com>
2025-03-10 22:52:34 -0500

    completado orm

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from flask import Flask
+from models import db
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///pokemon.db"
+Migrate(app,db)
+db.init_app(app) #establecer la coneccion con flask y sqlalchemy
+
+
+
 
 if __name__=="__main__":
     app.run(host="localhost", port=5000, debug=True)
 
 
-    
+
 
 
 
--- models.py
+++ models.py
@@ -1,5 +1,66 @@
-from flask_sqlalchemy import S
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import String, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column,relationship
 
 
+db = SQLAlchemy()
 
 
+class Pokedex(db.Model):
+    __tablename__ = 'pokedex'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    nombre: Mapped[str] = mapped_column(nullable=False)
+    numero: Mapped[int] = mapped_column()
+    tipo: Mapped[str] = mapped_column()
+    
+    evolucion = db.relationship('evolucion')
+    ataque = db.relationship('ataque')
+    Caracteristica = db.relationship('caracteristica')
+
+class Evolucion(db.Model):
+    __tablename__ = 'evolucion'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    nameEvo: Mapped[str] = mapped_column(String(100),nullable=False)
+    evolucion1: Mapped[str] = mapped_column()
+    evolucion2: Mapped[str] = mapped_column()
+    evolucion3: Mapped[str] = mapped_column()
+    pokemon_id:Mapped[int]=mapped_column(ForeignKey('pokedex.id'))
+
+    ataque = db.relationship('ataque')
+
+class Ataque(db.Model):
+    __tablename__ = 'ataque'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    defensa:Mapped[str] = mapped_column()
+    velocidad: Mapped[int] = mapped_column()
+    ataque_especial:Mapped[str] = mapped_column()
+    poke_id: Mapped[int]=mapped_column(ForeignKey('pokedex.id'))
+    evolucion: Mapped[int]=mapped_column(ForeignKey('evolucion.id'))
+
+    debilidad = db.relationship('debilidad')
+
+class Debilidad(db.Model):
+    __tablename__= 'debilidad'
+
+    id:Mapped[int] = mapped_column(primary_key=True)
+    devilidad1:Mapped[str] = mapped_column()
+    devilidad2:Mapped[str] = mapped_column()
+    devilidad3:Mapped[str] = mapped_column()
+
+class Caracteristica(db.Model):
+    __tablename__ = 'caracteristica'
+
+    id: Mapped[int]=mapped_column(primary_key=True)
+    altura: Mapped[int] = mapped_column()
+    peso:Mapped[int] = mapped_column()
+    sexo: Mapped[str] = mapped_column()
+    habilidad:Mapped[str] = mapped_column()
+    devilidad:Mapped[int] = mapped_column(ForeignKey('debilidad.id'))
+    pokemon:Mapped[int] = mapped_column(ForeignKey('pokedex.id'))
+    evo_id:Mapped[int] = mapped_column(ForeignKey('evolucion.id'))
+
+
+ 
